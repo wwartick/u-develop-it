fc6ca92de194e34149b1d8f287e5a1621bc9e4db
wwartick <willwartick@gmail.com>
2022-10-06 18:16:28 -0700

    called to database

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,11 +8,27 @@ const app = express();
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //Your MySQL username,
+        user:'root',
+        //Your MySQL password
+        password: 'Zabuza123!',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+    );
+
 
 app.get('/', (req,res) => {
     res.json({
             message: "Hello World"             
     });
+});
+
+db.query(`SELECT * FROM candidates`, (err, rows)=>{
+    console.log(rows);
 });
 
 app.use((req,res) => {
